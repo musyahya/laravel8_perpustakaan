109509b41565a24e9e24729e6a8a2fcbbf6ec5bd
musyahya <mananabdul37654@gmail.com>
2021-06-10 14:51:59 +0700

    laravel permission spatie

--- config/app.php
+++ config/app.php
@@ -175,6 +175,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        Spatie\Permission\PermissionServiceProvider::class,
+
     ],
 
     /*
