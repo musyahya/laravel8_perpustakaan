ae57a9b5562efd2de3770d1f6972a8c8dc776f97
musyahya <mananabdul37654@gmail.com>
2021-06-10 16:37:56 +0700

    migration

---
+++ database/migrations/2021_06_10_092904_create_rak_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRakTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rak', function (Blueprint $table) {
+            $table->id();
+            $table->string('rak');
+            $table->string('baris');
+            $table->foreignId('kategori_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rak');
+    }
+}
---
+++ database/migrations/2021_06_10_092940_create_buku_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBukuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buku', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('slug');
+            $table->string('sampul');
+            $table->string('penulis');
+            $table->foreignId('penerbit_id');
+            $table->foreignId('kategori_id');
+            $table->foreignId('rak_id');
+            $table->integer('stok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buku');
+    }
+}
