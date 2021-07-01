4ca141bbf433c9d44b68c976c373cdbb679e6fa7
musyahya <mananabdul37654@gmail.com>
2021-07-01 10:12:42 +0700

    merapikan route

--- routes/web.php
+++ routes/web.php
@@ -12,41 +12,35 @@ use App\Http\Controllers\Petugas\TransaksiController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', PeminjamBukuController::class);
 
 Auth::routes();
 
-Route::get('/cek-role', CekRoleController::class)->middleware('auth');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/cek-role', CekRoleController::class);
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
+    // role admin dan petugas
+    Route::middleware(['role:admin|petugas'])->group(function () {
+        Route::get('/dashboard', function () {
+            return view('petugas/dashboard');
+        });
 
-Route::middleware(['auth', 'role:admin|petugas'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('petugas/dashboard');
+        Route::get('/kategori', KategoriController::class);
+        Route::get('/rak', RakController::class);
+        Route::get('/penerbit', PenerbitController::class);
+        Route::get('/buku', BukuController::class);
+        Route::get('/transaksi', TransaksiController::class);
     });
 
-    Route::get('/kategori', KategoriController::class);
-    Route::get('/rak', RakController::class);
-    Route::get('/penerbit', PenerbitController::class);
-    Route::get('/buku', BukuController::class);
-    Route::get('/transaksi', TransaksiController::class);
-});
+    // role peminjam
+    Route::middleware(['role:peminjam'])->group(function () {
+        Route::get('/keranjang', KeranjangController::class);
+    });
 
-Route::middleware(['auth', 'role:peminjam'])->group(function () {
-    Route::get('/keranjang', KeranjangController::class);
-});
-
-Route::middleware(['auth', 'role:admin'])->group(function () {
-    Route::get('/user', UserController::class);
+    // role admin
+    Route::middleware(['role:admin'])->group(function () {
+        Route::get('/user', UserController::class);
+    });
 });
